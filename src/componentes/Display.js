61ffd3cce1c5bc811b14aaa3166deb5cabae251f
Sandra Quero <sanquesan@gmail.com>
2021-10-20 20:39:20 +0200

    Añadimos test (fallido, no me sale :( ), del componente Display

--- src/componentes/Display.js
+++ src/componentes/Display.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import TelefonoContext from "../context/TelefonoContext";
 import PropTypes from "prop-types";
+import "./Display.css";
 
 const Display = () => {
   const { numerosPantalla } = useContext(TelefonoContext);
