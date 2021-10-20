e94ada0dff611006d44a154d34cced8fb3542652
Sandra Quero <sanquesan@gmail.com>
2021-10-20 20:15:50 +0200

    Creamos componente Display, componente TelefonoContext

---
+++ src/componentes/Display.js
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import TelefonoContext from "../context/TelefonoContext";
+import PropTypes from "prop-types";
+
+const Display = () => {
+  const { numerosPantalla } = useContext(TelefonoContext);
+  return <span className="number">{numerosPantalla}</span>;
+};
+
+Display.propTypes = {
+  numerosPantalla: PropTypes.array.isRequired,
+};
+
+export default Display;
---
+++ src/context/TelefonoContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+const TelefonoContext = createContext();
+TelefonoContext.displayName = "TelefonoContext";
+
+export default TelefonoContext;
